6081df495c886dc925c8474d425070904e0b260e
Sandra Chumpitaz <chumpi_84@hotmail.com>
2021-07-14 19:18:03 -0500

    configuration spring

--- src/main/kotlin/pe/upc/vacunapp/ServerletInitializer.kt
+++ src/main/kotlin/pe/upc/vacunapp/ServerletInitializer.kt
@@ -3,10 +3,8 @@ package pe.upc.vacunapp
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
-class ServletInitializer : SpringBootServletInitializer() {
-
+class ServerletInitializer : SpringBootServletInitializer() {
 	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
 		return application.sources(VacunappApplication::class.java)
 	}
-
 }
